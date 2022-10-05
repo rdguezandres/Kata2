6d9838cb168b92f9fb26a9ba676313be7e65a81d
rdguezandres <andres.rodriguez.pineiro@rai.usc.es>
2022-10-05 11:03:54 +0200

    Kata 2 Version 2

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,7 @@ public class Main {
 
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         for (int i = 0; i < data.length; i++) {
-            if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            } else {
-                histogram.put(data[i], 1);
-            }
+            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
         }
 
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
