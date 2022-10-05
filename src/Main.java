df55809ead60b2e110d8043412695e676d30ca99
rdguezandres <andres.rodriguez.pineiro@rai.usc.es>
2022-10-05 11:20:51 +0200

    Kata 2 Version 5

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] data = {9, 2, 3, 4, 5, 8, 1, 2, 3, 9, 5, 3, 6, 2, 7, 3, 6, 0};
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
 
         HistogramGenerator histogramGenerator = new HistogramGenerator(data);
-        Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
+        Map<String, Integer> histogram = histogramGenerator.getHistogram();
 
-        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+        for (Map.Entry<String, Integer> entry : histogram.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
 
